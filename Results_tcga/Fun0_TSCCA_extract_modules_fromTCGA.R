6c7243a3d16e8071caea819eec5cd60cf454547e
wenwen <minwenwen07@163.com>
2020-11-16 15:38:14 +0800

    Add files via upload

---
+++ Results_tcga/Fun0_TSCCA_extract_modules_fromTCGA.R
@@ -0,0 +1,30 @@
+library("irlba")
+library("ggplot2")
+library("cowplot")
+library("openxlsx")
+
+# load("Data_tcga/TCGA_Tensor_Data.RData")
+load("../My_Data/1_tcga_data/TCGA_Tensor_Data.RData")
+
+# TSCCA function
+source('TSCCA.R') 
+
+# parameters of TSCCA
+ku = 10
+kv = 100
+kw = 20
+J  = 50 # 50 modules
+
+# Test to miRNA-gene data
+ptm = proc.time()
+out = TSCCA(DiffExp_Tensor, ku, kv, kw, J)
+time1 = proc.time() - ptm; print(time1)
+ptm = proc.time()
+
+# save
+tscca.parameter = list(ku=ku,kv=kv,kw=kw,J=J)
+tscca.out = out
+rm(DiffExp_Tensor)
+save.image(file = "Result_ku10_kv100_kw20_J50.RData")
+
+
